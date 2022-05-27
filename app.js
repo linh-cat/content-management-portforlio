caae53d9711fc402ac85c57d713d57be815faa26
Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2022-05-27 20:22:53 +0700

    example

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (_req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
